6506fef0f2f9db5bce402fc7ed06bdfb8a4d9693
SemTheLegend <semmathe@gmail.com>
2022-08-10 10:59:54 -0700

    fixed cdir,c

--- cdir.c
+++ cdir.c
@@ -96,14 +96,12 @@ void cd_previous(data_shell *datash)
 {
 	char pwd[PATH_MAX];
 	char *p_pwd, *p_oldpwd, *cp_pwd, *cp_oldpwd;
-OBOB
+
 	getcwd(pwd, sizeof(pwd));
-OBOBOB	cp_pwd = _strdup(pwd);
-
-OBOBOBOBOBOB	p_oldpwd = _getenv("OLDPWD", datash->_environ);
-
-OBOBOB	if (p_oldpwd == NULL)
-OBOBOBOBOBOB		cp_oldpwd = cp_pwd;
+	cp_pwd = _strdup(pwd);
+	p_oldpwd = _getenv("OLDPWD", datash->_environ);
+	if (p_oldpwd == NULL)
+		cp_oldpwd = cp_pwd;
 	else
 		cp_oldpwd = _strdup(p_oldpwd);
 
